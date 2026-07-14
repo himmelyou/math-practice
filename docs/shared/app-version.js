de746502a968726d555aa2543c7646a1bad4dac6
himmelyou <himmelyou@gmail.com>
2026-07-14 22:43:08 +0800

    训练模式自由选关

--- docs/shared/app-version.js
+++ docs/shared/app-version.js
@@ -3,7 +3,7 @@
  */
 (function (g) {
   g.JML_APP_VERSION = "v1.6.5";
-  g.JML_BUILD_ID = "20260714f";
-  g.JML_CACHE_TAG = "53";
+  g.JML_BUILD_ID = "20260714g";
+  g.JML_CACHE_TAG = "54";
   g.JML_ADMIN_STATIC_REV = "35";
 })(typeof window !== "undefined" ? window : globalThis);
